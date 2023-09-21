0c11eed6dff2933c6b24dc0a706c2ba4caa91eff
filipecsoares <filipecarlossoares@gmail.com>
2023-09-21 17:46:49 -0300

    refactor: extracting the argument parser

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parce_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
     println!("Here is the contents: \n\n{contents}");
+}
+
+fn parce_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
