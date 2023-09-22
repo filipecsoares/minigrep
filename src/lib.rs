d088fe22e25c470ed2e6950c8c97f216ff967438
filipecsoares <filipecarlossoares@gmail.com>
2023-09-22 12:57:54 -0300

    refactor: splitting code into a library crate

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("Here is the contents: \n\n{contents}");
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("You must specify a query string and a file path");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+}
