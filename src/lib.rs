69727e29750fecd1f84ec0266f373552f98a1610
filipecsoares <filipecarlossoares@gmail.com>
2023-09-23 08:33:06 -0300

    feat: search lines  with query in content

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,28 @@ impl Config {
         Ok(Config { query, file_path })
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search_one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+}
